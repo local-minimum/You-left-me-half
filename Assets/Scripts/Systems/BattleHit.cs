42242eee0be0da5ed0fbc55468724cd658eaf612
Martin Zackrisson <martin.zackrisson@fripost.org>
2023-04-06 18:30:15 +0200

    Battle system evaluates attacks

---
+++ Assets/Scripts/Systems/BattleHit.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHit : MonoBehaviour
+{
+    [SerializeField]
+    Color Normal;
+
+    [SerializeField]
+    Color Critical;
+
+    [SerializeField]
+    Color Fail;
+
+    [SerializeField]
+    int normalSize = 32;
+
+    [SerializeField]
+    int critSize = 42;
+
+    [SerializeField]
+    int failSize = 24;
+    
+    [SerializeField]
+    float lifeTime = 0.8f;
+
+    [SerializeField]
+    AnimationCurve alphaOverLifetime;
+
+    [SerializeField]
+    AnimationCurve offsetOverLifeTime;
+
+    [SerializeField]
+    TMPro.TextMeshProUGUI ui;
+
+    [SerializeField]
+    Rect shape;
+
+    public string Text
+    {
+        get => ui.text;
+        set { ui.text = value;  }
+    }
+
+    Color startColor;
+
+    public AttackMode AttackMode
+    {
+        set
+        {
+            switch (value)
+            {
+                case AttackMode.CritFail:
+                case AttackMode.Fail:
+                    startColor = Fail;
+                    break;
+                case AttackMode.Normal:
+                    startColor = Normal;
+                    break;
+                case AttackMode.CritSuccess:
+                    startColor = Critical;
+                    break;
+            }
+
+            ui.color = startColor;
+        }
+    }
+    
+    public void SetHit(int amount, AttackMode mode)
+    {
+        Text = amount.ToString();
+        AttackMode = mode;
+    }
+
+    void SetPosition(float t)
+    {
+        var yOffset = offsetOverLifeTime.Evaluate(t);
+        var anchor = new Vector2(x, yOffset);
+        var rt = (RectTransform)transform;
+        rt.anchorMin = anchor;
+        rt.anchorMax = anchor;
+        rt.offsetMin = shape.min;
+        rt.offsetMax = shape.max;
+    }
+
+    void SetColor(float t)
+    {
+        ui.color = startColor.Clone(alphaOverLifetime.Evaluate(t));
+    }
+        
+
+    float x;
+    float t0;
+
+    private void OnEnable()
+    {
+        x = Random.value;
+        SetPosition(0);
+        t0 = Time.timeSinceLevelLoad;
+    }
+
+    private void Update()
+    {
+        var t = (Time.timeSinceLevelLoad - t0) / lifeTime;
+        if (t > 1f)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        SetPosition(t);
+        SetColor(t);
+    }
+}
--- Assets/Scripts/Systems/BattleMaster.cs
+++ Assets/Scripts/Systems/BattleMaster.cs
@@ -1,22 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+
+public delegate void HitPlayer(int amount);
+public delegate void HitMonster(string monsterId, int amount);
+
 
 public class BattleMaster : MonoBehaviour
 {
+    public static event HitPlayer OnHitPlayer;
+    public static event HitMonster OnHitMonster;
+
+    [SerializeField]
+    BattleHit hitPrefab;
+
+    List<BattleHit> uiHits = new List<BattleHit>();
+
+    private void Start()
+    {
+        uiHits.AddRange(GetComponentsInChildren<BattleHit>());
+    }
+
+    BattleHit GetFreeHitUI()
+    {
+        var hit = uiHits.FirstOrDefault(hit => !hit.gameObject.activeSelf);
+
+        if (hit == null)
+        {
+            hit = Instantiate(hitPrefab);
+            hit.transform.SetParent(transform);
+            uiHits.Add(hit);
+        }
+        return hit;
+    }
+
     private void OnEnable()
     {
         AttackInventoryHUD.OnAttack += AttackInventoryHUD_OnAttack;
+        PlayerController.OnPlayerMove += PlayerController_OnPlayerMove;
     }
 
     private void OnDisable()
     {
         AttackInventoryHUD.OnAttack -= AttackInventoryHUD_OnAttack;
+        PlayerController.OnPlayerMove -= PlayerController_OnPlayerMove;
+    }
+
+    Vector3Int playerPosition;
+    FaceDirection playerLookDirection;
+
+    private void PlayerController_OnPlayerMove(Vector3Int position, FaceDirection lookDirection)
+    {
+        playerPosition = position;
+        playerLookDirection = lookDirection;
+    }
+
+    private string GetMonsterTarget(Attack attack)
+    {
+        return null;
     }
 
     private void AttackInventoryHUD_OnAttack(Attack attack)
     {
         var type = attack.GetAttack(out int amount);
-        Debug.Log($"{type}: {amount}");
+        var target = GetMonsterTarget(attack);
+
+        if (target != null)
+        {
+            OnHitMonster(target, amount);
+        }
+        
+        var hit = GetFreeHitUI();
+        hit.SetHit(amount, type);
+        hit.gameObject.SetActive(true);
     }
 }
